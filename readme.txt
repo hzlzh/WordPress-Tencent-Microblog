3238c9ad2d76be3e7325fb583fedfdba1baf3100
hzlzh <hzlzh.dev@gmail.com>
2011-03-31 03:26:23 +0000

    Add new project Wordpress Tencent Microblog

---
+++ readme.txt
@@ -0,0 +1,90 @@
+=== Wordpress Tencent Microblog ===
+Contributors: hzlzh
+Donate link: http://zlz.im/wordpress-tencent-microblog/
+Tags: 腾讯微博,QQ,微博,腾讯,同步
+Requires at least: 2.7
+Tested up to: 3.0.4
+Stable tag: 1.0.9
+
+显示腾讯微博发言的插件，Oauth登录认证，安全可靠，采取缓存机制，官方API，支持1-10条发言输出。
+
+== Description ==
+
+显示腾讯微博发言的插件，Oauth认证，即可从 <a href="http://q.hzlzh.com/">API</a> 处获得消息，显示在wordpress侧边栏上，安全可靠。采用了缓存机制，可自定义刷新时间，不占用站点加载速度。可以在[外观]--[小工具]中调用，也可以在任意位置使用`<?php display_tencent('username=you-ID&number=5'); ?>` 调用。
+
+== Installation ==
+
+1. 上传 `wordpress-tencent-microblog`插件到 `/wp-content/plugins/` 目录
+2. 在Wordpress后台控制面板"插件(Plugins)"菜单下激活"wordpress-tencent-microblog"
+3. 在Wordpress后台控制面板"外观(Appearance)->小工具(Widgets)"下使用`腾讯微博`
+4. 首次使用需激活API，点击后前往http://q.hzlzh.com/认证！
+
+== Frequently Asked Questions ==
+
+= 我不想在侧边栏调用，而是在任意位置可以么？怎么做？ =
+
+可以！需要激活插件后，在任意位置使用`<?php display_tencent('username=you-ID&number=5'); ?>`调用即可，只需要修改`you-ID` 为你的腾讯微博帐号，`5`是你想要展示的条数。
+
+
+
+= 使用之前必须先Oauth认证登录吗？ =
+
+是的第一次使用，需要认证，一次认证终身使用。
+
+= 如果我的HOST不支持copy()函数怎么办？ =
+
+可以去除缓存功能，直接抓取API即可，参见 <a href="http://zlz.im/wordpress-tencent-microblog/">使用说明</a>
+
+== Screenshots ==
+
+1. 使用侧边栏[小工具]调用的效果
+2. 后台截图
+
+== Changelog ==
+
+= V 1.0.9 =
+*使用Oauth认证得到API来获取信息
+*修改少许代码兼容新版API
+*重新使用 http://q.hzlzh.com/为API
+
+= V 1.0.7 =
+*增加了对copy()，file_get_contents等函数的验证，在网站主机不支持饿情况下，会提示原因。
+*新增加了一个判断更新的因素，当抓取文件为空时，从新抓取。
+*增加了file()函数抓取方式
+*去掉手动注释部分
+*原API 地址 http://q.hzlzh.com/重新可用！
+
+= V 1.0.5 =
+*修改了几个重要的注释
+*提供了WIN主机无法使用copy()函数的解决方案注释
+*修改了一些细节
+
+= V 1.0.3 =
+*现在支持 1至10 条的发言调用
+*修复了有实名认证的用户的显示错误BUG
+*加入了 CSS 样式，预留自定义接口
+
+= V 1.0.0  =
+*在Wordpress中同步显示腾讯微博中的发言
+*无需登录，采取民间API，安全可靠
+*由于使用缓存，本插件目录需要有写入权限
+*目前调取显示数量为1条，更多需要API的支持，尽在下一个版本
+
+== Upgrade Notice ==
+= V 1.0.9 =
+使用全新腾讯官方API Oauth认证机制，安全可靠，高速！
+
+= V 1.0.7 =
+加入了新的缓存判断，可以有效避免空抓的现象出现
+
+= V 1.0.6 =
+为了减少API 被禁止的可能，使用新的API地址，http://q.hzlzh.com/wordpress/
+
+= V 1.0.5 =
+修改了几个重要的注释，提供了WIN主机无法使用copy()函数的解决方案注释
+
+= 1.0.3 =
+本版本已经完善了10条以内信息的完善，并且预留了CSS样式名
+
+= 1.0.0 =
+这个是第一个版本，由于API限制只能调用1发言
---
+++ wordpress-tencent-microblog.php
@@ -0,0 +1,118 @@
+<?php
+/*
+Plugin Name: Wordpress Tencent Microblog
+Plugin URI: http://zlz.im/wordpress-tencent-microblog/
+Description:显示腾讯微博发言的插件，无需密码，安全可靠。采用了缓存机制，自定义刷新时间，不占用站点加载速度。可以在[外观]--[小工具]中调用，或者在任意位置使用 <code>&lt;?php display_tencent('username=you-ID&number=5'); ?&gt;</code> 调用。
+Version: 1.0.9
+Author: hzlzh
+Author URI: http://zlz.im
+
+*/
+//如果有遇到问题，请到http://zlz.im/wordpress-tencent-microblog/ 得到服务支持！
+if ( ! defined( 'WP_PLUGIN_URL' ) )
+      define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );//获得plugins网页路径
+if ( ! defined( 'WP_PLUGIN_DIR' ) )
+      define( 'WP_PLUGIN_DIR', WP_CONTENT_URL. '/plugins' );//获得plugins直接路径
+
+//展示函数
+function display_tencent($args = ''){
+	$default = array(
+		'username'=>'your-ID',
+		'number'=>'1',
+		'api'=>'http://q.hzlzh.com/',
+		'time'=>'3600');
+	$r = wp_parse_args($args,$default);
+	extract($r);
+	$url = $api.$username;
+	if(function_exists("copy")){
+	$e = WP_PLUGIN_DIR.'/wordpress-tencent-microblog/'.$username.'.json';
+	if ( !is_file($e) || (time() - filemtime($e)) > $time|| filesize($e) < 1000){
+	//当缓存不存在或超过 $time 时更新,或者得到文件大小小于1000
+	copy($url, $e);}//拷贝到本地，一般主机都支持这个函数,需要目录的写入权限
+	$jsonObject = file_get_contents($e);
+}else{
+	if(function_exists("file_get_contents")){
+	$jsonObject = file_get_contents($url);
+}else{
+	if(function_exists( "file")){
+	$f=file($url);
+	for($i=0;$i<count($f);$i++){
+		$jsonObject.=$f[$i];
+	}
+}else{
+echo "<div>该主机不支持本插件，由于禁用了copy()或file_get_contents()等函数，请在http://zlz.im/wordpress-tencent-microblog/ 告知出错原因，便于我维护更新插件！</div>";
+		}
+	}
+}
+	$decodedArray = json_decode($jsonObject, true);
+
+		echo '<ul style="list-style-type:none;">';
+		for($i = 0;$i <$number;$i++){
+			echo '<li><div class="microblog"><a href="http://t.qq.com/'.$username.'" rel="external nofollow" title="来自 腾讯微博" target="_blank"><img class="microblog-ico" style="float:left;padding-right:3px;" alt="腾讯微博" src="'.WP_PLUGIN_URL.'/wordpress-tencent-microblog/txwb.gif" /></a><span class="microblog-content">'.$decodedArray [data][info][$i][origtext].'</span>  <span class="microblog-from" style="font-style:italic;">-'.date("Y/m/d", $decodedArray [data][info][$i][timestamp]).' 来自 '.$decodedArray [data][info][$i][from].'-</span></div></li>';
+		}
+		echo '</ul>';
+}
+
+//扩展类 WP_Widget
+class TencentMicroblog extends WP_Widget
+{
+	//定义后台面板展示文字
+	function TencentMicroblog(){
+		$widget_des = array('classname'=>'wordpress-tencent-microblog','description'=>'在博客显示腾讯微博的发言');
+		$this->WP_Widget(false,'腾讯微博',$widget_des);
+	}
+
+	//定义widget后台选项
+	function form($instance){
+		$instance = wp_parse_args((array)$instance,array(
+		'title'=>'腾讯微博',
+		'username'=>'your-ID',
+		'api'=>'http://q.hzlzh.com/',
+		'number'=>1,
+		'time'=>'3600'));
+		$title = htmlspecialchars($instance['title']);
+		$username = htmlspecialchars($instance['username']);
+		$api = htmlspecialchars($instance['api']);
+		$number = htmlspecialchars($instance['number']);
+		$time = htmlspecialchars($instance['time']);
+		echo '<p><b>首次使用需激活API--></b><a target="_ablank" href="http://q.hzlzh.com/q/">点此激活认证</a></p><p style="color:#FF3333;">任何问题请@我的微博<a target="_ablank" href="http://t.qq.com/hzlzh_com">hzlzh_com</a> 反馈</p><p><label for="'.$this->get_field_name('title').'">侧边栏标题:<input style="width:200px;" id="'.$this->get_field_id('title').'" name="'.$this->get_field_name('title').'" type="text" value="'.$title.'" /></label></p>
+		<p><label for="'.$this->get_field_name('username').'">用户名:<input style="width:200px;" id="'.$this->get_field_id('username').'" name="'.$this->get_field_name('username').'" type="text" value="'.$username.'" /></label></p>
+		<p><label for="'.$this->get_field_name('api').'">API地址:<input style="width:200px;" id="'.$this->get_field_id('api').'" name="'.$this->get_field_name('api').'" type="text" value="'.$api.'" /></label></p>
+		<p><label for="'.$this->get_field_name('number').'">显示数量:<input style="width:200px" id="'.$this->get_field_id('number').'" name="'.$this->get_field_name('number').'" type="text" value="'.$number.'" /></label></p>
+		<p><label for="'.$this->get_field_name('time').'">缓存时间:<input style="width:200px" id="'.$this->get_field_id('time').'" name="'.$this->get_field_name('time').'" type="text" value="'.$time.'" />秒</label></p>';
+	}
+	
+	//更新函数
+	function update($new_instance,$old_instance){
+		$instance = $old_instance;
+		$instance['title'] = strip_tags(stripslashes($new_instance['title']));
+		$instance['username'] = strip_tags(stripslashes($new_instance['username']));
+		$instance['api'] = strip_tags(stripslashes($new_instance['api']));
+		$instance['number'] = strip_tags(stripslashes($new_instance['number']));
+		$instance['time'] = strip_tags(stripslashes($new_instance['time']));
+		return $instance;
+	}
+
+	//显示函数
+	function widget($args,$instance){
+		extract($args);
+		$title = apply_filters('widget_title',empty($instance['title']) ? '&nbsp;' : $instance['title']);
+		$username = empty($instance['username']) ? 'your-ID' : $instance['username'];
+		$number = empty($instance['number']) ? 1 : $instance['number'];
+		$api = empty($instance['api']) ? 'http://q.hzlzh.com/' : $instance['api'];
+		$time = empty($instance['time']) ? 3600 : $instance['time'];
+		
+		echo $before_widget;
+		echo $before_title . $title . $after_title;
+		display_tencent("username=$username&number=$number&api=$api&time=$time");
+		echo $after_widget;
+	}
+}
+
+//注册widget
+function TencentMicroblogInit(){
+	register_widget('TencentMicroblog');
+}
+
+add_action('widgets_init','TencentMicroblogInit');
+?>
